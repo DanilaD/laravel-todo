2bf35c7424a98f8878df67166f6c18fd850a97d5
DanilaD <d6174173844@gmail.com>
2022-08-12 19:50:34 -0400

    add additional files

--- resources/views/livewire/add.blade.php
+++ resources/views/livewire/add.blade.php
@@ -1,1 +1,53 @@
-<?php
+<div class="panel">
+    <div class="panel-heading">
+        <h3 class="panel-title text-capitalize text-center">
+            @if($add === 'add')
+                Add New Task
+            @else
+                Edit Task
+            @endif
+        </h3>
+    </div>
+    <div class="panel-body">
+        <hr/>
+            <div class="row">
+                <div class="col-md-12 mt-1">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input class="form-control" wire:model="task.name" />
+                        @error('task.name') <span class="text-danger small">{{$message}}</span> @enderror
+                    </div>
+                </div>
+                <div class="col-md-6 mt-1">
+                    <div class="form-group">
+                        <label>Project</label>
+                        <select class="form-control" wire:model="task.project_id">
+                            <option></option>
+                            @foreach($projects as $project)
+                                <option value="{{$project->id}}">{{$project->name}}</option>
+                            @endforeach
+                        </select>
+                        @error('task.project_id') <span class="text-danger small">{{$message}}</span> @enderror
+                    </div>
+                </div>
+                <div class="col-md-6 mt-1">
+                    <div class="form-group">
+                        <label>Priority</label>
+                        <div class="form-group">
+                            <input class="form-control" type="number" wire:model="task.priority"/>
+                        </div>
+                        @error('task.priority') <span class="text-danger small">{{$message}}</span> @enderror
+                    </div>
+                </div>
+        </div>
+        <hr/>
+        <div class="row">
+            <div class="col-12">
+                <div class="form-group">
+                    <button class="btn btn-success" wire:click="store()">Submit</button>
+                    <button class="btn btn-outline-danger" wire:click="cancel()">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/view.blade.php
+++ resources/views/livewire/view.blade.php
@@ -1,1 +1,57 @@
-<?php
+<div class="panel">
+    <div class="panel-heading">
+        <h3 class="panel-title text-capitalize text-center">Task Management</h3>
+        <div class="panel-actions">
+            <button type="button" class="btn btn-outline-primary btn-sm mb-2" wire:click="add()">Create Task</button>
+        </div>
+    </div>
+    <div class="panel-body">
+        <hr/>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-md-6">
+                    <label>Search by Name</label>
+                    <input class="form-control" wire:model="search.name" />
+                </div>
+                <div class="col-md-6">
+                    <label>Search by Project</label>
+                    <select class="form-control" wire:model="search.project_id">
+                        <option></option>
+                        @foreach($projects as $project)
+                            <option value="{{$project->id}}">{{$project->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <div>
+            <ul wire:sortable="updateTaskOrder">
+                @foreach ($data as $v)
+                    <li wire:sortable.item="{{ $v->id }}" wire:key="task-{{ $v->id }}">
+                        <div class="row">
+                            <div class="col-md-6">
+                                {{$v->name}}
+                            </div>
+                            <div class="col-md-2">
+                                {{$v->Project->name}}
+                            </div>
+                            <div class="col-md-2">
+                                {{\Carbon\Carbon::parse($v->created_at)->format('m/d/Y h:i A')}}
+                            </div>
+                            <div class="col-md-2">
+                                <div x-data="{ open: false }">
+                                    <button class="btn btn-outline-success btn-sm" wire:click="edit({{$v->id}})">edit</button>
+                                    <button @click="open = true" x-show="!open" class="btn btn-outline-danger btn-sm">delete</button>
+                                    <button x-show="open" @click.away="open=false" x-cloak type="button" class="btn btn-danger btn-sm" wire:click="delete('{{ $v->id }}')">confirm</button>
+                                </div>
+                            </div>
+                        </div>
+                        <hr/>
+
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</div>
