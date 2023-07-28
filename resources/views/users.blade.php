e5e75b17ee8aad4c88adf99ad8cbe9dfd46cf09d
Danila Dolmatov <danila.dolmatov@lightningstep.com>
2023-07-28 01:06:00 -0400

    removed site

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -17,7 +17,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header bg-info text-white">Laravel Datatables Pagination with Search and Sort - NiceSnippets.com</div>
+                    <div class="card-header bg-info text-white">Laravel Datatables Pagination with Search and Sort</div>
                     <div class="card-body">
 @if (session('status'))
     <div class="alert alert-success" role="alert">
