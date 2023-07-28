0d8577a22f77925c84242531afdebd25d09be8e3
Danila Dolmatov <danila.dolmatov@lightningstep.com>
2023-07-28 01:09:44 -0400

    change routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', \App\Http\Livewire\ToDoComponent::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('todo', \App\Http\Livewire\ToDoComponent::class);
 
 Route::get('users','\App\Http\Controllers\UserController@index');
 Route::get('/users/getusers/','\App\Http\Controllers\UserController@getUser')->name('users.getusers');
